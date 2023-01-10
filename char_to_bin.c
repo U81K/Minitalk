4748b81bb18e52990dc38080fbeae1fd9ffc1e73
8il4l <73673899+8il4l@users.noreply.github.com>
2023-01-10 12:25:28 +0100

    client

--- char_to_bin.c
+++ char_to_bin.c
@@ -1,39 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
+/*   char_to_bin.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bgannoun <bgannoun@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/03 20:31:41 by bgannoun          #+#    #+#             */
-/*   Updated: 2023/01/10 11:50:17 by bgannoun         ###   ########.fr       */
+/*   Created: 2023/01/10 09:55:46 by bgannoun          #+#    #+#             */
+/*   Updated: 2023/01/10 10:21:16 by bgannoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <signal.h>
 #include <stdio.h>
-#include <unistd.h>
 
-void sigint_handler(int signu) {
-  // Handle the SIGINT signal
-  printf("%d\n", signu);
+void decimal_to_binary(int decimal, int binary[]) {
+    int i = 0;
+    while (decimal > 0) {
+        binary[i] = decimal % 2;
+        decimal = decimal / 2;
+        i++;
+    }
+    while (i < 8) {
+        binary[i] = 0;
+        i++;
+    }
 }
 
-// int main() {
-//   // Set up the SIGINT signal handler
-//   signal(SIGINT, sigint_handler);
-//   // Wait for a signal to be received
-//   pause();
-//   return 0;
-// }
+int main() {
+    int width = 8;
+    int binary[width];
+    decimal_to_binary(97, binary);
 
-int main(void)
-{
-	pid_t	id;
-
-	id = getpid();
-	printf("%d\n", id);
-	signal(SIGUSR2, sigint_handler);
-	pause();
-	return (0);
+	int i;
+	i = 7;
+    while (i >= 0) 
+        printf("%d", binary[i--]);
+    printf("\n");
+    return 0;
 }
--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: bgannoun <bgannoun@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 20:32:06 by bgannoun          #+#    #+#             */
-/*   Updated: 2023/01/03 20:32:06 by bgannoun         ###   ########.fr       */
+/*   Updated: 2023/01/10 11:43:03 by bgannoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,12 +63,85 @@ int	atoi(char *a)
 	return (res * sign);
 }
 
+// void char_to_bin(pid_t id, char c) 
+// {
+// 	int i;
+// 	int bit;
+
+// 	i = 7;
+//     while (i >= 0) 
+// 	{
+//         // Shift c to the right by i bits and mask it with 1
+//         // to get the value of the i-th bit
+//         bit = (c >> i) & 1;
+
+//         // Print the bit
+//         // printf("%d", bit);
+// 		if (bit)
+// 			kill(id, SIGUSR1);
+// 			// printf("1");
+// 		else
+// 			kill(id, SIGUSR2);
+// 			// printf("0");
+// 		i--;
+//     }
+// 	// printf("\n");
+// }
+
+
+void decimal_to_binary(int decimal, int binary[], pid_t id) 
+{
+    int i;
+
+	i = 0;
+    while (decimal > 0) 
+	{
+        binary[i] = decimal % 2;
+        decimal = decimal / 2;
+        i++;
+    }
+    while (i < 8) 
+	{
+        binary[i] = 0;
+        i++;
+    }
+	i = 7;
+	while (i >= 0)
+	{
+		if(binary[i] == 0)
+			kill(id, SIGUSR1);
+		else if (binary[i] == 1)
+			kill(id, SIGUSR2);
+		i--;
+	}
+	printf("\n");
+}
+
+void send_str(pid_t id, char *str)
+{
+	int i;
+	int bin[8];
+
+	i = 0;
+	while (str[i])
+	{
+		decimal_to_binary(str[i], bin, id);
+		i++;
+	}
+}
+
 int	main(int ac, char **av)
 {
 	pid_t id;
+	// int bin[8];
 
+	if (ac != 3)
+		return (1);
 	id = atoi(av[1]);
-	kill(id, SIGUSR1);
+	send_str(id, av[2]);
+	// decimal_to_binary('a', bin);
+	// send_str(id, av[2]);
+	// kill(id, SIGUSR2);
 	// printf("%d\n", id);
 	return (0);
 }
