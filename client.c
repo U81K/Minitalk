e854e6f8b29ce98dafd238b1d6d28d117be40e5a
8il4l <73673899+8il4l@users.noreply.github.com>
2023-01-11 00:43:34 +0100

    d

--- client.c
+++ client.c
@@ -6,19 +6,13 @@
 /*   By: bgannoun <bgannoun@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 20:32:06 by bgannoun          #+#    #+#             */
-/*   Updated: 2023/01/10 11:43:03 by bgannoun         ###   ########.fr       */
+/*   Updated: 2023/01/11 00:40:03 by bgannoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <unistd.h>
 #include <stdio.h>
-
-// int main() {
-//   // Send the SIGINT signal to process with ID 12345
-//   kill(224, SIGINT);
-//   return 0;
-// }
 
 void ft_putchar(int c)
 {
@@ -63,32 +57,6 @@ int	atoi(char *a)
 	return (res * sign);
 }
 
-// void char_to_bin(pid_t id, char c) 
-// {
-// 	int i;
-// 	int bit;
-
-// 	i = 7;
-//     while (i >= 0) 
-// 	{
-//         // Shift c to the right by i bits and mask it with 1
-//         // to get the value of the i-th bit
-//         bit = (c >> i) & 1;
-
-//         // Print the bit
-//         // printf("%d", bit);
-// 		if (bit)
-// 			kill(id, SIGUSR1);
-// 			// printf("1");
-// 		else
-// 			kill(id, SIGUSR2);
-// 			// printf("0");
-// 		i--;
-//     }
-// 	// printf("\n");
-// }
-
-
 void decimal_to_binary(int decimal, int binary[], pid_t id) 
 {
     int i;
@@ -113,8 +81,8 @@ void decimal_to_binary(int decimal, int binary[], pid_t id)
 		else if (binary[i] == 1)
 			kill(id, SIGUSR2);
 		i--;
+		usleep(300);
 	}
-	printf("\n");
 }
 
 void send_str(pid_t id, char *str)
@@ -133,7 +101,6 @@ void send_str(pid_t id, char *str)
 int	main(int ac, char **av)
 {
 	pid_t id;
-	// int bin[8];
 
 	if (ac != 3)
 		return (1);
--- server.c
+++ server.c
@@ -6,26 +6,26 @@
 /*   By: bgannoun <bgannoun@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 20:31:41 by bgannoun          #+#    #+#             */
-/*   Updated: 2023/01/10 11:50:17 by bgannoun         ###   ########.fr       */
+/*   Updated: 2023/01/11 00:39:27 by bgannoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
-void sigint_handler(int signu) {
-  // Handle the SIGINT signal
-  printf("%d\n", signu);
+void signal_handler(int signum) 
+{
+	if (signum == SIGUSR1)
+	{
+		write(1,"0",1);
+	} 
+	else if (signum == SIGUSR2)
+	{
+		write(1,"1",1);
+	}
 }
-
-// int main() {
-//   // Set up the SIGINT signal handler
-//   signal(SIGINT, sigint_handler);
-//   // Wait for a signal to be received
-//   pause();
-//   return 0;
-// }
 
 int main(void)
 {
@@ -33,7 +33,10 @@ int main(void)
 
 	id = getpid();
 	printf("%d\n", id);
-	signal(SIGUSR2, sigint_handler);
-	pause();
+	signal(SIGUSR1, signal_handler);
+	signal(SIGUSR2, signal_handler);
+	while (1){
+		pause();
+	}
 	return (0);
 }
